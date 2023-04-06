cf3c8d1feb2466ba30aeb873ae227dde14e9e5fe
Yen <s810643@gmail.com>
2023-04-06 23:45:13 +0800

    Add schedule model

---
+++ model/schedule.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+interface IKeyValue {
+  key: string;
+  value: any;
+}
+
+// Interface
+interface ISchedule {
+  stockNo: string;
+  stockName: string;
+  year: string;
+  month: string;
+  date: string;
+  cashDividen: number;
+  others: IKeyValue[]; //{key,value}
+  updateDate: string; //yyyymmdd
+  sourceType: string;
+}
+
+// Schema
+const ScheduleSchema: Schema = new Schema({
+  stockNo: String,
+  stockName: String,
+  year: String,
+  month: String,
+  date: String,
+  cashDividen: Number,
+  others: Array,
+  updateDate: String,
+  sourceType: String,
+});
+
+const ScheduleModel: Model<ISchedule> = mongoose.model<ISchedule>('Schedule', ScheduleSchema);
+
+export { ScheduleModel, ISchedule, IKeyValue };
--- route/stock.ts
+++ route/stock.ts
@@ -1,12 +1,15 @@
 import express, { Router, NextFunction, Request, Response } from 'express';
 
 import { StockListModel, IStockList } from '../model/stockList';
+import { ScheduleModel, ISchedule } from '../model/schedule';
 
 const router: Router = express.Router();
 
 router.get('/list', async (req: Request, res: Response, next: NextFunction) => {
-  const list: IStockList[] = await StockListModel.find();
-  res.json(list);
+  //const list: IStockList[] = await StockListModel.find();
+
+  const schedule: ISchedule[] = await ScheduleModel.find({ sourceType: '除權息預告', year: '2023' });
+  res.json({ schedule });
 });
 
 export default router;
