b08ccbe78cbd2711f389eaa8a0859b102a90327e
Yen <s810643@gmail.com>
2024-02-20 00:32:01 +0800

    Add parsePosts and loop to previous page

--- service/pttStockInfo.ts
+++ service/pttStockInfo.ts
@@ -1,26 +1,87 @@
 import { getHTML } from '../utility/requestCore';
 
-async function parse() {
-  var $ = await getHTML('https://www.ptt.cc/bbs/Stock/index.html');
-  $('.r-ent .title a').each((index, element) => {
-    const title = $(element).text();
-    const link = $(element).attr('href');
+interface PostInfo {
+  tag: string | null;
+  title: string;
+  href: string | null;
+  author: string | null;
+  date: string | null;
+}
+const domain = 'https://www.ptt.cc';
 
-    console.log(`Title: ${title}`);
-    console.log(`Link: ${link}`);
+async function handler() {
+  //TODO: figure out what is last process record then stop
+  var page = '';
+  var continueFlag = true;
+  var posts: PostInfo[] = [];
+  var stopCount = 3;
+  var currentCount = 0;
+  while (continueFlag && currentCount < stopCount) {
+    currentCount++;
+    var url = `${domain}/bbs/Stock/index${page || ''}.html`;
+    console.log(`process url ${url}`);
+    var $ = await getHTML(url);
+    var posts = parsePosts($);
+    posts.concat(posts);
+    page = getPreviousPageIndex($);
+    if (page == '6933') {
+      continueFlag = false;
+    }
+  }
+
+  // Filter posts where tag is equal to '標的'
+  const filteredPosts = posts.filter((post) => post.tag === '標的');
+
+  // Iterate through filteredPosts and log each post to the console
+  filteredPosts.forEach((post, index) => {
+    console.log(`Post ${index + 1}:`);
+    console.log(`Date: ${post.date}`);
+    console.log(`Tag: ${post.tag}`);
+    console.log(`Title: ${post.title}`);
+    console.log(`Href: ${domain}/${post.href}`);
+    console.log(`Author: ${post.author}`);
     console.log('------------------');
-    getPageLink($);
   });
 }
 
-function getPageLink($: cheerio.Root) {
-  const link = $('a.btn.wide').attr('href');
+function parsePosts($: cheerio.Root): PostInfo[] {
+  const posts: PostInfo[] = [];
 
-  // Extract page number from the link using regular expression
-  const match = link && link.match(/index(\d+)\.html/);
-  const pageNumber = match ? match[1] : null;
+  $('div.r-ent').each((index, element) => {
+    const titleElement = $(element).find('div.title a');
+    const tag = titleElement.text().match(/\[(.*?)\]/)?.[1] || null;
+    const title = titleElement
+      .text()
+      .replace(/\[.*?\]/, '')
+      .trim();
+    const href = titleElement.attr('href') || null;
+    const author = $(element).find('div.author').text() || null;
+    const date = $(element).find('div.date').text().trim() || null;
 
-  console.log(`Page Number: ${pageNumber}`);
+    const postInfo: PostInfo = {
+      tag,
+      title,
+      href,
+      author,
+      date,
+    };
+
+    posts.push(postInfo);
+  });
+
+  return posts;
 }
 
-parse();
+function getPreviousPageIndex($: cheerio.Root): string {
+  //const link = $('a.btn.wide').attr('href');
+  const link = $('a.btn.wide')
+    .filter((_, element) => $(element).text().includes('上頁'))
+    .attr('href');
+  // Extract page number from the link using regular expression
+  const match = link && link.match(/index(\d+)\.html/);
+  const index = match ? match[1] : null;
+
+  return index as string;
+}
+
+handler();
