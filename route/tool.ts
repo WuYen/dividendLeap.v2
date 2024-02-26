87ef4fe297d095fb014cea2bb4104dc5f74ffe35
Yen <s810643@gmail.com>
2024-02-26 23:37:07 +0800

    Add cron and tool route

---
+++ route/tool.ts
@@ -0,0 +1,9 @@
+import express, { Router, NextFunction, Request, Response } from 'express';
+
+const router: Router = express.Router();
+
+router.get('/health', async (req: Request, res: Response, next: NextFunction) => {
+  res.json({ msg: `I'm alive` + new Date() });
+});
+
+export default router;
