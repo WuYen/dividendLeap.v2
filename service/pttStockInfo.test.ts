8380de805af9e211d55908f5b29a4d027d1a969d
Yen <s810643@gmail.com>
2024-03-01 23:27:19 +0800

    Integrate the old and new ptt services and add an endpoint to retrieve the latest 50 posts.

--- service/pttStockInfo.test.ts
+++ service/pttStockInfo.test.ts
@@ -1,5 +1,6 @@
 import service from './pttStockInfo'; // Replace with the correct path to your file
 import { IPostInfo } from '../model/PostInfo';
+import { fastFindNewPosts, parseId } from './pttStockInfo';
 
 describe('isHighlightAuthor', () => {
   // Test cases
@@ -16,6 +17,18 @@ describe('isHighlightAuthor', () => {
   test('Null author is not recognized', () => {
     const nullAuthor: string | null = null;
     expect(service.isHighlightAuthor(nullAuthor)).toBe(false);
+  });
+});
+
+describe('test utility', () => {
+  it('shouild get id', async () => {
+    var result = parseId('https://www.ptt.cc/bbs/Stock/M.1709040700.A.47E.html');
+    expect(typeof result).toBe('number');
+    expect(result).toEqual(1709040700);
+
+    result = parseId('/bbs/Stock/M.1709040700.A.47E.html');
+    expect(typeof result).toBe('number');
+    expect(result).toEqual(1709040700);
   });
 });
 
@@ -77,4 +90,156 @@ describe('processMessage', () => {
   });
 
   // Add more tests as needed
+});
+
+const deletedPost = `<div class="r-ent">
+<div class="nrec"></div>
+<div class="title">
+
+  (本文已被刪除) [kcbug123]
+
+</div>
+<div class="meta">
+  <div class="author">-</div>
+  <div class="article-menu">
+    
+  </div>
+  <div class="date"> 3/01</div>
+  <div class="mark"></div>
+</div>
+</div>`;
+
+describe('fastFindNewArticles', () => {
+  it('should find new articles', () => {
+    // Sample input for onlineArticles
+    const onlineArticles: IPostInfo[] = [
+      {
+        tag: 'News',
+        title: 'Article 1',
+        href: '/article/1',
+        author: 'John Doe',
+        date: '2022-01-01',
+        batchNo: 2,
+        id: 1,
+      },
+      {
+        tag: 'Technology',
+        title: 'Article 2',
+        href: '/article/2',
+        author: 'Jane Smith',
+        date: '2022-01-02',
+        batchNo: 2,
+        id: 2,
+      },
+      {
+        tag: 'Sports',
+        title: 'Article 3',
+        href: '/article/3',
+        author: 'Bob Johnson',
+        date: '2022-01-03',
+        batchNo: 2,
+        id: 3,
+      },
+    ];
+
+    // Sample input for savedArticles
+    const savedArticles: IPostInfo[] = [
+      {
+        tag: 'News',
+        title: 'Article 1',
+        href: '/article/1',
+        author: 'John Doe',
+        date: '2022-01-01',
+        batchNo: 1,
+        id: 1,
+      },
+      {
+        tag: 'Technology',
+        title: 'Article 2',
+        href: '/article/2',
+        author: 'Jane Smith',
+        date: '2022-01-02',
+        batchNo: 1,
+        id: 2,
+      },
+    ];
+
+    // Call the function with sample input
+    const newArticles = fastFindNewPosts(onlineArticles, savedArticles);
+
+    // Assert the result
+    expect(newArticles).toEqual([
+      {
+        tag: 'Sports',
+        title: 'Article 3',
+        href: '/article/3',
+        author: 'Bob Johnson',
+        date: '2022-01-03',
+        batchNo: 2,
+        id: 3,
+      },
+    ]);
+  });
+
+  it('handle delete articles', () => {
+    // Sample input for onlineArticles
+    const onlineArticles: IPostInfo[] = [
+      {
+        tag: 'News',
+        title: 'Article 1',
+        href: '/article/1',
+        author: 'John Doe',
+        date: '2022-01-01',
+        batchNo: 2,
+        id: 1,
+      },
+      {
+        tag: 'Sports',
+        title: 'Article 3',
+        href: '/article/3',
+        author: 'Bob Johnson',
+        date: '2022-01-03',
+        batchNo: 2,
+        id: 3,
+      },
+    ];
+
+    // Sample input for savedArticles
+    const savedArticles: IPostInfo[] = [
+      {
+        tag: 'News',
+        title: 'Article 1',
+        href: '/article/1',
+        author: 'John Doe',
+        date: '2022-01-01',
+        batchNo: 1,
+        id: 1,
+      },
+      {
+        tag: 'Technology',
+        title: 'Article 2',
+        href: '/article/2',
+        author: 'Jane Smith',
+        date: '2022-01-02',
+        batchNo: 1,
+        id: 2,
+      },
+    ];
+
+    // Call the function with sample input
+    const newArticles = fastFindNewPosts(onlineArticles, savedArticles);
+
+    // Assert the result
+    expect(newArticles).toEqual([
+      {
+        tag: 'Sports',
+        title: 'Article 3',
+        href: '/article/3',
+        author: 'Bob Johnson',
+        date: '2022-01-03',
+        batchNo: 2,
+        id: 3,
+      },
+    ]);
+  });
 });
