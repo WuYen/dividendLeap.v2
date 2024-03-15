d31067a2ac283cd6c291198a0538470dd53869ea
“vince” <vince-yp.wu@nanshan.com.tw>
2024-03-15 17:17:57 +0800

    update test case for real example

--- service/pttStockInfo.test.ts
+++ service/pttStockInfo.test.ts
@@ -33,28 +33,44 @@ describe('test utility', () => {
 
   it('should return true if post title contains "re:" as a separate word', () => {
     const post: IPostInfo = {
-      title: 'Some re: post',
-      tag: '標的',
-      href: null,
-      author: null,
-      date: null,
-      batchNo: 0,
-      id: 0,
+      id: 123,
+      tag: '請益',
+      title: 'Re:  關於大量未實現損益',
+      href: '/bbs/Stock/M.1709218876.A.B93.html',
+      author: 'cgagod',
+      date: '2/29',
+      batchNo: 1709220361794,
     };
     expect(isRePosts(post)).toBe(true);
   });
 
   it('should return false if post title not contains Re: ', () => {
     const post: IPostInfo = {
-      title: 'Some : post',
+      id: 456,
       tag: '標的',
-      href: null,
-      author: null,
-      date: null,
-      batchNo: 0,
-      id: 0,
+      title: 'NVDA尾盤5分鐘發生什麼事情',
+      href: '/bbs/Stock/M.1709243920.A.902.html',
+      author: 'wen17',
+      date: '3/01',
+      batchNo: 1709257208580,
     };
+
     expect(isRePosts(post)).toBe(false);
+  });
+
+  it('should not send re post', () => {
+    const post: IPostInfo = {
+      id: 123,
+      tag: '標的',
+      title: 'Re:  2880 華南金 多',
+      href: '/bbs/Stock/M.1710472460.A.BDE.html',
+      author: 'cgagod',
+      date: '2/29',
+      batchNo: 1709220361794,
+    };
+    var shouldSend = !isRePosts(post);
+    expect(isRePosts(post)).toBe(true);
+    expect(shouldSend).toBe(false);
   });
 });
 
