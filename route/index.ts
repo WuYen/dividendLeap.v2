16e03b82d00d409558b939729783ffe9be2561e4
“vince” <vince-yp.wu@nanshan.com.tw>
2024-03-26 16:01:04 +0800

    Route enhance to allow multiple route map to same router

--- route/index.ts
+++ route/index.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 import stockRoute from './stock';
 import jobRoute from './job';
-import lineRoute from './line';
+import lineRoute from './lineNotify';
 import yahooRoute from './yahoo';
 import pttStockInfo from './pttStockInfo';
 import tool from './tool';
@@ -9,39 +9,24 @@ import tool from './tool';
 const router = express.Router();
 
 interface IRoute {
-  path: string;
+  path: string | string[];
   route: Router;
 }
 
 const defaultIRoute: IRoute[] = [
-  {
-    path: '/stock',
-    route: stockRoute,
-  },
-  {
-    path: '/job',
-    route: jobRoute,
-  },
-  {
-    path: '/line',
-    route: lineRoute,
-  },
-  {
-    path: '/yahoo',
-    route: yahooRoute,
-  },
-  {
-    path: '/ptt',
-    route: pttStockInfo,
-  },
-  {
-    path: '/tool',
-    route: tool,
-  },
+  { path: '/stock', route: stockRoute },
+  { path: '/job', route: jobRoute },
+  { path: ['/line', '/line/notify'], route: lineRoute },
+  { path: '/line/message', route: lineRoute },
+  { path: '/yahoo', route: yahooRoute },
+  { path: '/ptt', route: pttStockInfo },
+  { path: '/tool', route: tool },
 ];
 
 defaultIRoute.forEach((route) => {
   router.use(route.path, route.route);
 });
+
+router.use(['/line/notify', '/line'], lineRoute);
 
 export default router;
