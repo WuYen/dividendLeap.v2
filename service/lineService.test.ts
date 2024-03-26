9ab8e5c1fd448bd0d6763f2200f3903f18b7bc7b
Yen <s810643@gmail.com>
2024-03-27 01:01:11 +0800

    Fix damn line middleware signature not working

---
+++ service/lineService.test.ts
@@ -0,0 +1,13 @@
+// import { MiddlewareConfig, middleware } from '@line/bot-sdk';
+// import config from '../utility/config';
+
+// describe('test fugle service', () => {
+//   it('get price by date ranges', async () => {
+//     const middlewareConfig: MiddlewareConfig = {
+//       channelAccessToken: config.LINE_CHANNEL_ACCESS_TOKEN,
+//       channelSecret: config.LINE_CHANNEL_SECRET,
+//     };
+
+//     var result = middleware(middlewareConfig);
+//   });
+// });
