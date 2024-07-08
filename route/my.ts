8813daee6444a4704ecbb0f5b8e3ed63f1d86447
Yen <s810643@gmail.com>
2024-07-08 22:26:23 +0800

    Add return statement in route

--- route/my.ts
+++ route/my.ts
@@ -15,32 +15,32 @@ router.get('/post/:id/favorite', async (req: IAuthRequest, res: Response, next: 
     const postId = req.params.id;
     const userId = req.user?.id || '';
     await toggleFavoritePost(userId, postId);
-    res.sendSuccess(200, { message: '收藏成功' });
+    return res.sendSuccess(200, { message: '收藏成功' });
   } catch (error) {
-    res.sendError(500, { message: '收藏失敗' });
+    return res.sendError(500, { message: '收藏失敗' });
   }
 });
 
 router.get('/posts', async (req: IAuthRequest, res: Response, next: NextFunction) => {
   const userId = req.user?.id || '';
   var result = await getLast50Posts();
-  res.sendSuccess(200, { data: result });
+  return res.sendSuccess(200, { data: result });
 });
 
 router.get('/posts/favorite', async (req: IAuthRequest, res: Response, next: NextFunction) => {
   const userId = req.user?.id || '';
   var result = await getFavoritePosts(userId);
-  res.sendSuccess(200, { data: result });
+  return res.sendSuccess(200, { data: result });
 });
 
 router.get('/author/:id/like', async (req: IAuthRequest, res: Response, next: NextFunction) => {
   try {
     const authorId = req.params.id;
     const authorInfo = await addLikeToAuthor(authorId);
-    res.sendSuccess(200, { message: 'Liked', data: authorInfo });
+    return res.sendSuccess(200, { message: 'Liked', data: authorInfo });
   } catch (error) {
     console.error('Error while liking author:', error);
-    res.sendError(500, { message: 'Internal server error' });
+    return res.sendError(500, { message: 'Internal server error' });
   }
 });
 
@@ -1509,10 +1509,10 @@ router.get('/authors/rank', async (req: IAuthRequest, res: Response, next: NextF
         combinedRank: 24,
       },
     ];
-    res.sendSuccess(200, { data });
+    return res.sendSuccess(200, { data });
   } catch (error) {
     console.error('Error while liking author:', error);
-    res.sendError(500, { message: 'Internal server error' });
+    return res.sendError(500, { message: 'Internal server error' });
   }
 });
 export default router;
