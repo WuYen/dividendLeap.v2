6e6b4665fd760e8af3fcb9cfa4b7d3557581cea9
PEI <s810643@gmail.com>
2024-07-19 16:55:01 +0800

    Fix build fail

--- job/jobRunner.ts
+++ job/jobRunner.ts
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 import config from '../utility/config';
-import { filterPositive, getDataAndProcessToResult } from '../service/rankingService';
 
 mongoose.connect(config.MONGODB_URI).then(async () => {
-  //await filterPositive();
   mongoose.disconnect();
 });
