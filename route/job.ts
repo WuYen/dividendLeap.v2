2a4c5aa0fd0bee7ed3c5ca2f0af4964202cf38ac
PEI <s810643@gmail.com>
2024-07-19 17:10:54 +0800

    Add update rank job

--- route/job.ts
+++ route/job.ts
@@ -1,8 +1,14 @@
 import express, { Router, NextFunction, Request, Response } from 'express';
+import { processAndUpdateAuthorStats } from '../service/authorStatsService';
 const router: Router = express.Router();
 
 router.get('/dayinfo', async (req: Request, res: Response, next: NextFunction) => {
   //await updateDayInfo();
+  return res.sendSuccess(200, { message: 'success' });
+});
+
+router.get('/update/rank', async (req: Request, res: Response, next: NextFunction) => {
+  await processAndUpdateAuthorStats();
   return res.sendSuccess(200, { message: 'success' });
 });
 
