71a9655e993eb7f9e11e8cc3b8b44fc8582c9a59
PEI <s810643@gmail.com>
2024-10-04 16:50:45 +0800

    Add function to convert data into format of tune model file for openAI

--- job/jobRunner.ts
+++ job/jobRunner.ts
@@ -1,6 +1,20 @@
 import mongoose from 'mongoose';
 import config from '../utility/config';
+import { convertToJSONL, parts } from '../service/geminiAIService';
+import * as fs from 'fs';
 
-mongoose.connect(config.MONGODB_URI).then(async () => {
-  mongoose.disconnect();
+// mongoose.connect(config.MONGODB_URI).then(async () => {
+//   mongoose.disconnect();
+// });
+
+var jsonOutput = convertToJSONL(parts);
+console.log(jsonOutput);
+
+// Write JSON output to file
+fs.writeFile('output.jsonl', jsonOutput, (err) => {
+  if (err) {
+    console.error('Error writing file:', err);
+  } else {
+    console.log('JSON output written to output.json');
+  }
 });
