d94973e49e21cdcaaf6ceafb708b732a23c2b8c5
PEI <s810643@gmail.com>
2024-10-08 17:41:42 +0800

    add setting in index

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import mongoose from 'mongoose';
+import webPush from 'web-push';
 
 import config from './utility/config';
 import middleware from './utility/middleware';
@@ -9,6 +10,16 @@ const app: Express = express();
 
 app.use(middleware);
 app.use(router);
+
+// 設定 web-push 的 VAPID 詳細資訊
+const publicVapidKey = process.env.VAPID_PUBLIC_KEY!;
+const privateVapidKey = process.env.VAPID_PRIVATE_KEY!;
+
+webPush.setVapidDetails(
+  'mailto:your-email@example.com', // 替換為你的電子郵件
+  publicVapidKey,
+  privateVapidKey
+);
 
 let server: any;
 mongoose.connect(config.MONGODB_URI).then(() => {
